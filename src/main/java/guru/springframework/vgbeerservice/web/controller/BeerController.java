562043ab445e5e442510185d966fc4b651f983b3
vitor <vitorgilcsgmail.com>
2022-08-24 10:22:14 +0100

    AddingJunit 5 Controller Test - problem running the tests. For now will be commented

--- src/main/java/guru/springframework/vgbeerservice/web/controller/BeerController.java
+++ src/main/java/guru/springframework/vgbeerservice/web/controller/BeerController.java
@@ -1,7 +1,6 @@
 package guru.springframework.vgbeerservice.web.controller;
 
 import guru.springframework.vgbeerservice.web.model.BeerDto;
-import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
