209599f538601e0f8ce3c6aca9f6cdefaf8187d3
vitor <vitorgilcsgmail.com>
2022-09-02 10:58:42 +0100

    Jackson Configuration

--- src/main/java/guru/springframework/vgbeerservice/web/model/BeerDto.java
+++ src/main/java/guru/springframework/vgbeerservice/web/model/BeerDto.java
@@ -1,5 +1,6 @@
 package guru.springframework.vgbeerservice.web.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -27,13 +28,16 @@ public class BeerDto {
     private Integer version;
 
     @Null
+    @JsonFormat(pattern = "yyyy-MM.dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
     private OffsetDateTime createdDate;
 
     @Null
+    @JsonFormat(pattern = "yyyy-MM.dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
     private OffsetDateTime lastModifiedDate;
 
     @NotBlank
     private String beerName;
+
     @NotNull
     private BeerStyleEnum beerStyle;
 
@@ -41,6 +45,7 @@ public class BeerDto {
     @NotNull
     private Long upc;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
     @Positive
     @NotNull
     private BigDecimal price;
