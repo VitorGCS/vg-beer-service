b6f1b5d5082dcd1cd0765853291608daa5969788
vitor <vitorgilcsgmail.com>
2022-09-02 11:31:01 +0100

    Adding methods for controller

---
+++ src/main/java/guru/springframework/vgbeerservice/services/BeerService.java
@@ -0,0 +1,15 @@
+package guru.springframework.vgbeerservice.services;
+
+import guru.springframework.vgbeerservice.web.model.BeerDto;
+
+
+import java.util.UUID;
+
+public interface BeerService {
+
+    BeerDto getById(UUID beerId);
+
+    BeerDto saveNewBeer(BeerDto beerDto);
+
+    BeerDto updateBeer(UUID beerId, BeerDto beerDto);
+}
---
+++ src/main/java/guru/springframework/vgbeerservice/web/controller/NotFoundException.java
@@ -0,0 +1,4 @@
+package guru.springframework.vgbeerservice.web.controller;
+
+public class NotFoundException extends RuntimeException{
+}
