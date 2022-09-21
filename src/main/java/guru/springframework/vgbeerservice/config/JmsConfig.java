7c95e04aeb0d990b579d8dc7766a49b573eeeda8
vitor <vitorgilcsgmail.com>
2022-09-21 16:09:42 +0100

    Configure JMS

---
+++ src/main/java/guru/springframework/vgbeerservice/config/JmsConfig.java
@@ -0,0 +1,22 @@
+package guru.springframework.vgbeerservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+@Configuration
+public class JmsConfig {
+
+    /*public static final String MY_QUEUE = "my-hello-world";
+    public static final String MY_SEND_RCV_QUEUE = "replybacktome";
+*/
+    @Bean // Serialize message content to json using TextMessage
+    public MessageConverter messageConverter() {
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+        return converter;
+    }
+}
