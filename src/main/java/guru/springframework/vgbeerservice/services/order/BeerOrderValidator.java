d5b0e53f1e8aff31270b891a5b10f27aa3845f08
vitor <vitorgilcsgmail.com>
2022-09-28 15:19:35 +0100

    Handle Validate Order Request

---
+++ src/main/java/guru/springframework/vgbeerservice/services/order/BeerOrderValidator.java
@@ -0,0 +1,27 @@
+package guru.springframework.vgbeerservice.services.order;
+
+import guru.sfg.brewery.model.BeerOrderDto;
+import guru.springframework.vgbeerservice.repositories.BeerRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class BeerOrderValidator {
+    private final BeerRepository beerRepository;
+
+    public Boolean validateOrder(BeerOrderDto beerOrder){
+        AtomicInteger beersNotFound = new AtomicInteger();
+
+        beerOrder.getBeerOrderLines().forEach( beerOrderLineDto -> {
+            if(beerRepository.findByUpc(beerOrderLineDto.getUpc())== null){
+                beersNotFound.incrementAndGet();
+            }
+        });
+        return  beersNotFound.get() == 0;
+    }
+}
