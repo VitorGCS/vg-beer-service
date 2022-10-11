88c15a03d39999e7eb5fdffbb469b004fc79894c
vitor <vitorgilcsgmail.com>
2022-10-11 01:29:39 +0100

    Configure RESTTemplate for HTTP Basic Authentication

--- src/main/java/guru/springframework/vgbeerservice/services/inventory/BeerInventoryServiceRestTemplateImpl.java
+++ src/main/java/guru/springframework/vgbeerservice/services/inventory/BeerInventoryServiceRestTemplateImpl.java
@@ -2,6 +2,7 @@ package guru.springframework.vgbeerservice.services.inventory;
 
 import guru.springframework.vgbeerservice.services.inventory.model.BeerInventoryDto;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Profile;
@@ -16,7 +17,7 @@ import java.util.Objects;
 
 @Profile("!local-discovery")
 @Slf4j
-@ConfigurationProperties(prefix = "sfg.brewery", ignoreUnknownFields = false)
+@ConfigurationProperties(prefix = "sfg.brewery", ignoreUnknownFields = true)
 @Component
 public class BeerInventoryServiceRestTemplateImpl implements BeerInventoryService {
 
@@ -26,12 +27,17 @@ public class BeerInventoryServiceRestTemplateImpl implements BeerInventoryServic
 
     private String beerInventoryServiceHost;
 
+
     public void setBeerInventoryServiceHost(String beerInventoryServiceHost) {
         this.beerInventoryServiceHost = beerInventoryServiceHost;
     }
 
-    public BeerInventoryServiceRestTemplateImpl(RestTemplateBuilder restTemplateBuilder) {
-        this.restTemplate = restTemplateBuilder.build();
+    public BeerInventoryServiceRestTemplateImpl(RestTemplateBuilder restTemplateBuilder,
+                                                @Value("${sfg.brewery.inventory-user}") String inventoryUser,
+                                                @Value("${sfg.brewery.inventory-password}") String inventoryPassword) {
+        this.restTemplate = restTemplateBuilder
+                .basicAuthentication(inventoryUser,inventoryPassword)
+                .build();
     }
 
 /*    @Override
